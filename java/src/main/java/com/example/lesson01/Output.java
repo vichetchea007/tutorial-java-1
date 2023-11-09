529753aa17ab36a77bbc0c6f318a4eed6d461ebc
Chea Vichet <vichetchea@gmail.com>
2023-11-09 09:50:11 +0700

    Java Tutorial

---
+++ java/src/main/java/com/example/lesson01/Output.java
@@ -0,0 +1,12 @@
+package com.example.lesson01;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("1+2");
+        System.out.println(1+2);
+        System.out.println(7/2);
+        System.out.println(7.0/2);
+        System.out.println(7/2.0);
+        System.out.println(7.0/2.0);
+    }
+}
